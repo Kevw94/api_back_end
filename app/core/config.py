f015d5b49c0b6d963761eb976355ed25d94862cc
Kevw94 <pinerokevin@icloud.com>
2022-10-12 10:17:17 +0200

    add example for crud with route + model

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,7 @@
 from dotenv import dotenv_values
+import motor.motor_asyncio
 
 config = dotenv_values('./.env')
+
+client = motor.motor_asyncio.AsyncIOMotorClient(config["MONGO_URI"])
+db = client.twitter
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,13 +1,14 @@
 from app.core.config import config
 from fastapi import APIRouter
+from typing import List
+from app.crud.crud_users import get_all_users
+from app.models.users import UserModel
 
 router = APIRouter(
     prefix=f"{config['APP_PREFIX']}"
 )
 
 
-@router.get("/users", response_description="Add new user")
-def get_user():
-    return {"message": "user"}
-
-
+@router.get("/users", response_description="List all user", response_model=List[UserModel])
+async def get_user():
+	return await get_all_users()
