3845ff829e48522739ab5105ec0d3225214eee29
Kevw94 <pinerokevin@icloud.com>
2022-10-12 14:40:04 +0200

    implement jwt token auth in one file
    Feat:
        - implement jwt logic in crudauth
    
    WIP:
        - split this check in middleware + utils

--- app/crud/crud_auth.py
+++ app/crud/crud_auth.py
@@ -1,15 +1,50 @@
-import datetime
-from fastapi import Request
+from app.core.config import config
+from datetime import datetime, timedelta
 from app.core.config import db
-from app.models.auth import AuthModel
+from app.models.auth import AuthModel, LoginModel
+from passlib.context import CryptContext
+from jose import jwt
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-async def try_create_user(request: Request, user_auth: AuthModel):
-	created_at = datetime.datetime.now()
+async def try_create_user(user_auth: AuthModel):
+	hashed_password = get_password_hash(user_auth.password)
+	print(hashed_password)
+	created_at = datetime.now()
 	new_user = {
 		"username": user_auth.username,
-		"password": user_auth.password,
+		"password": hashed_password,
 		"created_at": created_at
 	}
 	db["users"].insert_one(new_user)
 	return { "success": new_user }
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+async def user_logged(user_login: LoginModel):
+	is_user_found = await db["users"].find_one({
+		"username": user_login.username
+	})
+	if is_user_found != None:
+		hashed_password = verify_password(user_login.password, is_user_found["password"])
+		if hashed_password:
+			return is_user_found
+		else:
+			return False
+	else:
+		return False
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, config["SECRET_KEY"], algorithm=config["ALGORITHM"])
+    return encoded_jwt
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,10 +1,12 @@
 from app.core.config import config
 from fastapi import APIRouter
 from fastapi import Request, Body
-from app.crud.crud_auth import try_create_user
+from fastapi import HTTPException, status
+from app.crud.crud_auth import create_access_token, try_create_user, user_logged
 from app.core.config import db
 
-from app.models.auth import AuthModel
+from app.models.auth import AuthModel, LoginModel
+from datetime import datetime, timedelta
 
 router = APIRouter(
     prefix=f"{config['APP_PREFIX']}"
@@ -12,5 +14,24 @@ router = APIRouter(
 
 
 @router.post("/signup", response_description="create user")
-async def create_user(request: Request, user_auth: AuthModel = Body(...)):
-	await try_create_user(request, user_auth)
+async def create_user(user_auth: AuthModel = Body(...)):
+	await try_create_user(user_auth)
+
+
+@router.post("/login", response_description="user Login")
+async def try_login(user_login: LoginModel = Body(...)):
+	user = await user_logged(user_login)
+	print(user)
+	if not user:
+		raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+	access_token_expires = timedelta(minutes=30)
+	access_token = create_access_token(data={"sub": user["username"]}, expires_delta=access_token_expires)
+	return {"access_token": access_token, "token_type": "bearer"}
+
+
+
+
