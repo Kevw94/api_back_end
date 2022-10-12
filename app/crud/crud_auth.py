93369ecbd435511b20c75c9f9a3e4a2ea450819b
Kevw94 <pinerokevin@icloud.com>
2022-10-12 22:16:16 +0200

    add condition to prevent from duplicating usernames in db

--- app/crud/crud_auth.py
+++ app/crud/crud_auth.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from app.core.config import db
 from app.core.security import get_password_hash, verify_password
 from app.models.auth import AuthModel, LoginModel
-from bson.json_util import dumps
+from fastapi import HTTPException, status
 
 
 async def try_create_user(user_auth: AuthModel):
@@ -11,19 +11,30 @@ async def try_create_user(user_auth: AuthModel):
 	Args:
 		user_auth (AuthModel): accept a username and pssword from the Body
 
+	Raises:
+		credentials_exception: user already exists in DB -> prevent duplicate
+
 	Returns:
 		success: 201
 	"""
-	hashed_password = get_password_hash(user_auth.password)
-	created_at = datetime.now()
-	new_user = {
-		"username": user_auth.username,
-		"password": hashed_password,
-		"created_at": created_at,
-		"disabled": False
-	}
-	db["users"].insert_one(new_user)
-	return { "success": 201 }
+	credentials_exception = HTTPException(
+		status_code=status.HTTP_401_UNAUTHORIZED,
+		detail="This username is already in use",
+	)
+	is_username_exists = await db["users"].find_one({"username": user_auth.username})
+	if is_username_exists != None:
+		raise credentials_exception
+	else:
+		hashed_password = get_password_hash(user_auth.password)
+		created_at = datetime.now()
+		new_user = {
+			"username": user_auth.username,
+			"password": hashed_password,
+			"created_at": created_at,
+			"disabled": False
+		}
+		db["users"].insert_one(new_user)
+		return { "success": 201 }
 
 
 
