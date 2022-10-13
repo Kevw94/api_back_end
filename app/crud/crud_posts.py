f78cc597536856bef4f8c018fb30ce707dc7a890
FERREIRA Luciano <ferreira.luciano@outlook.fr>
2022-10-13 11:24:36 +0200

    Implement delete get_me_posts

--- app/crud/crud_posts.py
+++ app/crud/crud_posts.py
@@ -3,14 +3,29 @@ import datetime
 from bson import ObjectId
 
 from app.core.config import db
-from app.models.posts import PostModel, PatchModel
+from app.models.posts import PostModel
 
 
 async def crud_get_all_posts():
+    """Function to get all posts from all users
+
+    Returns:
+        object: JSON of all posts
+
+    """
     return await db["posts"].find().to_list(500)
 
 
 async def crud_create_post(post: PostModel):
+    """Function to create a post and adding created_at field
+
+    Args:
+        post: accepts content field
+
+    Returns:
+        object: Success Message
+
+    """
     created_at = datetime.datetime.now()
     new_post = {
         "userId": post.userId,
@@ -22,7 +37,44 @@ async def crud_create_post(post: PostModel):
 
 
 async def crud_patch_post(post_id, patch):
+    """Function to edit the content of a precise post by its ID
+
+    Args:
+        post_id: ID of the targeted post
+        patch: accepted Model containing the new content
+
+    Returns:
+        object: Success message
+
+    """
     db['posts'].update_one({"_id": ObjectId(post_id)},
                            {"$set": {"content": patch.content}}
                            )
     return {"Successful": 'Updated post'}
+
+
+async def crud_get_me_posts(user_data):
+    """Function to get all posts by the current user
+
+    Args:
+        user_data: Current user data containing its ID
+
+    Returns:
+        object: JSON of all the current user's posts
+
+    """
+    posts = await db['posts'].find({'userId': user_data['_id']}).to_list(1000)
+    return posts
+
+
+async def crud_delete_post(post_id):
+    """Function to delete a precise post by its ID
+
+    Args:
+        post_id: ID of the targeted post
+
+    Returns:
+        object: Success message
+    """
+    await db['posts'].delete_one({'_id': ObjectId(post_id)})
+    return {"Successful": "Deletion successful"}
--- app/routers/posts.py
+++ app/routers/posts.py
@@ -1,10 +1,14 @@
 from bson import ObjectId
-from fastapi import Body
+from fastapi import Body, Depends
 from app.core.config import config
 from fastapi import APIRouter
 from typing import List
-from app.crud.crud_posts import crud_get_all_posts, crud_create_post, crud_patch_post
+
+from app.core.security import get_current_active_user
+from app.crud.crud_posts import crud_get_all_posts, crud_create_post, crud_patch_post, crud_get_me_posts, \
+    crud_delete_post
 from app.models.posts import PostModel, PatchModel
+from app.models.users import UserModel
 
 router = APIRouter(
     prefix=f"{config['APP_PREFIX']}"
@@ -24,3 +28,13 @@ async def create_post(post: PostModel = Body(...)):
 @router.patch("/posts/{post_id}", response_description="Post edited")
 async def patch_post(post_id: str, patch: PatchModel = Body(...)):
     return await crud_patch_post(post_id, patch)
+
+
+@router.get('/posts/me', response_description="List of your posts", response_model=List[PostModel])
+async def get_me_posts(current_user: UserModel = Depends(get_current_active_user)):
+    return await crud_get_me_posts(current_user)
+
+
+@router.delete('/posts/{post_id}', response_description="Post successfully deleted")
+async def delete_post(post_id: str):
+    return await crud_delete_post(post_id)
