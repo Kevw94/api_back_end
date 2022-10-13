203ea97df8bd6139391c2768b7c3284040099636
Kevw94 <pinerokevin@icloud.com>
2022-10-13 18:44:07 +0200

    implement patch commentary on post

--- app/routers/comments.py
+++ app/routers/comments.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter
 from typing import List
 
 from app.core.security import get_current_active_user
-from app.crud.crud_comments import insert_comment_in_db, try_get_comments_in_db
+from app.crud.crud_comments import insert_comment_in_db, try_get_comments_in_db, try_patch_comment_in_db
 from app.models.comments import CommentsModel
 from app.models.users import UserModel
 
@@ -24,3 +24,8 @@ async def create_comment(current_user: UserModel = Depends(get_current_active_us
 async def get_comment_from_post_id(post_id: str):
 	return await try_get_comments_in_db(post_id)
 
+
+@router.patch("/comments/{comments_id}")
+async def patch_comment_by_id(comments_id: str, patch_comments: CommentsModel = Body(...), current_user: UserModel = Depends(get_current_active_user)):
+	await try_patch_comment_in_db(comments_id, patch_comments, current_user)
+	return True
