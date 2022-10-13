ac8ea8de913a9e41db805813694752ef9704b079
Kevw94 <pinerokevin@icloud.com>
2022-10-13 10:39:55 +0200

    implement get_user_by_id for doing followings logical

--- app/crud/crud_users.py
+++ app/crud/crud_users.py
@@ -1,4 +1,20 @@
 from app.core.config import db
+from app.models.users import UserModel
+from bson import ObjectId
 
 async def get_all_users():
 	return await db["users"].find().to_list(100)
+
+
+async def try_change_username(modif_username: UserModel):
+	is_username_exists = await db["users"].find_one({'username': modif_username.username})
+	if is_username_exists == None:
+		await db["users"].update_one({"_id": ObjectId(modif_username.id)}, {"$set": {"username": modif_username.username}})
+
+
+async def try_find_user(user_profile_to_follow: UserModel):
+	find_to_follow = await db["users"].find_one({'_id': ObjectId(user_profile_to_follow)})
+	if find_to_follow == None:
+		raise Exception
+	else:
+		return find_to_follow
---
+++ app/routers/followers.py
@@ -0,0 +1,9 @@
+
+
+# created_at = datetime.now()
+# 		new_follower = {
+# 			"userId": current_user["_id"], 
+# 			"followingId": user_profile_to_follow,
+# 			"created_at": created_at
+# 		}
+# 		db["followers"].insert_one({ new_follower })
