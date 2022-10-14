c0934f21a4f17fd249b28fd4d9727b6dd88c1178
Kevw94 <pinerokevin@icloud.com>
2022-10-14 13:04:53 +0200

    add responses for comments

--- app/routers/comments.py
+++ app/routers/comments.py
@@ -14,7 +14,7 @@ router = APIRouter(
 )
 
 
-@router.post("/comments", response_description="Comment created")
+@router.post("/comments", response_description="Comment created", responses={201: {"content": {"Comment created"} }, 409: {"content": {"There were a problem with create comment try again"}}})
 async def create_comment(current_user: UserModel = Depends(get_current_active_user), create_comments: CommentsModel = Body(...)):
 	"""create a comment from a post with a post ID required in the body
 
@@ -29,7 +29,7 @@ async def create_comment(current_user: UserModel = Depends(get_current_active_us
 	return {"success": "Comment created"}
 
 
-@router.get("/comments/me", response_description="list of my commentaries", response_model=List[CommentsModel])
+@router.get("/comments/me", response_description="list of my commentaries", response_model=List[CommentsModel], responses={200: {"description": "list of comments of the user returned in an array" }})
 async def get_my_comments(current_user: UserModel = Depends(get_current_active_user)):
 	"""get all comments from the user 
 
@@ -43,7 +43,7 @@ async def get_my_comments(current_user: UserModel = Depends(get_current_active_u
 	return get_comments
 
 
-@router.get("/comments/posts/{post_id}",  dependencies=[Depends(get_current_active_user)], response_model=List[CommentsModel])
+@router.get("/comments/posts/{post_id}",  dependencies=[Depends(get_current_active_user)], response_model=List[CommentsModel],  responses={200: {"description": "list of comments related to the ID of the post" }})
 async def get_comment_from_post_id(post_id: str):
 	"""get comments from the posts ID
 
@@ -56,7 +56,7 @@ async def get_comment_from_post_id(post_id: str):
 	return await try_get_comments_in_db(post_id)
 
 
-@router.patch("/comments/{comments_id}")
+@router.patch("/comments/{comments_id}", responses={201: {"content": {"Comment updated"} }, 409: {"content": {"There were a problem with update try again"}}})
 async def patch_comment_by_id(comments_id: str, patch_comments: CommentsModel = Body(...), current_user: UserModel = Depends(get_current_active_user)):
 	"""patch comments with the comment id and the active user
 
@@ -72,7 +72,7 @@ async def patch_comment_by_id(comments_id: str, patch_comments: CommentsModel = 
 	return {"success": "Comment updated"}
 
 
-@router.delete("/comments/{comments_id}")
+@router.delete("/comments/{comments_id}", responses={201: {"content": {"Comment deleted"} }, 409: {"content": {"There were a problem with delete comment try again"}}})
 async def delete_comment_by_id(comments_id: str,  current_user: UserModel = Depends(get_current_active_user)):
 	"""delete comments with the comment id and the active user
 
