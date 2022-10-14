30cbd7e17b2d18c67fa31bc0b6d4d9507efa74c6
Kevw94 <pinerokevin@icloud.com>
2022-10-14 13:21:39 +0200

    implement routes for posts

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -14,7 +14,7 @@ router = APIRouter(
 )
 
 
-@router.get("/posts", response_description="List all posts", response_model=List[PostModel], dependencies=[Depends(get_current_active_user)])
+@router.get("/posts", response_description="List all posts", response_model=List[PostModel], dependencies=[Depends(get_current_active_user)], responses={200: {"description": "JSON of all posts"}})
 async def get_all_posts():
     """Gets all posts
 
@@ -24,7 +24,7 @@ async def get_all_posts():
     return await crud_get_all_posts()
 
 
-@router.post("/posts", response_description="Post created")
+@router.post("/posts", response_description="Post created", responses={201: {"content": {"Post created"}}, 409: {"content": {"Problem with creating post try again"}}})
 async def create_post(post: PostModel = Body(...), current_user: UserModel = Depends(get_current_active_user)):
     """Creates a post with the given body
 
@@ -37,7 +37,7 @@ async def create_post(post: PostModel = Body(...), current_user: UserModel = Dep
     return await crud_create_post(post, current_user)
 
 
-@router.patch("/posts/{post_id}", response_description="Post edited")
+@router.patch("/posts/{post_id}", response_description="Post edited", responses={201: {"content": {"Updated post"}}, 409: {"content": {"Problem with updating posts in db"}}})
 async def patch_post(post_id: str, patch: PatchModel = Body(...), current_user: UserModel = Depends(get_current_active_user)):
     """Edits the targeted post with given body
 
@@ -51,7 +51,7 @@ async def patch_post(post_id: str, patch: PatchModel = Body(...), current_user: 
     return await crud_patch_post(post_id, patch, current_user)
 
 
-@router.get('/posts/me', response_description="List of your posts", response_model=List[PostModel])
+@router.get('/posts/me', response_description="List of your posts", response_model=List[PostModel], responses={200: {"description": "JSON of all posts of the user"}})
 async def get_me_posts(current_user: UserModel = Depends(get_current_active_user)):
     """Gets the current user's posts
 
@@ -64,7 +64,7 @@ async def get_me_posts(current_user: UserModel = Depends(get_current_active_user
     return await crud_get_me_posts(current_user)
 
 
-@router.delete('/posts/{post_id}', response_description="Post successfully deleted")
+@router.delete('/posts/{post_id}', response_description="Post successfully deleted", responses={201: {"content": {"Deletion successful"}, 409: {"content": {"Problem with updating posts in db"}}}})
 async def delete_post(post_id: str, current_user: UserModel = Depends(get_current_active_user)):
     """Deletes the targeted post
 
